2a3188e245eb27cf031aad1a1f43b04f49a17131
mithilaNKD <136618846+mithilaNKD@users.noreply.github.com>
2023-07-31 23:40:45 +0530

    Add files via upload

---
+++ Web lab Sheet/Tut10/tute10Q1.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <title>tute10</tilte>
+    </head>
+    <body>
+    <?php
+
+    $x = 6;
+    $y = 4;
+
+    $sum_result = $x + $y;
+    $difference_result = $x - $y;
+    $product_result = $x * $y;
+    $division_result = $x / $y;
+    ?>
+
+    <h1>Simple Calculator</h1>
+    <p>Sum:
+        <?php echo $sum_result; ?>
+    </p>
+    <p>Difference:
+        <?php echo $difference_result; ?>
+    </p>
+    <p>Product:
+        <?php echo $product_result; ?>
+    </p>
+    <p>Division:
+        <?php echo $division_result; ?>
+    </p>
+</body>
+
+</html>
---
+++ Web lab Sheet/Tut10/tute10Q2.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+    <title>tute10Q2</title>
+</head>
+
+<body>
+    <h1>Count from 5 to 15:</h1>
+    
+        <?php
+        for ($i = 5; $i <= 15; $i++)
+        {
+            echo "$i";
+        }
+        ?>
+    
+</body>
+
+</html>
